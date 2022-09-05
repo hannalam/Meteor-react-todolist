98429ae7d9c7c150e994fb5b68a1063d306dea6d
hanna lam <63362052+hannalam@users.noreply.github.com>
2022-09-05 23:35:11 +0200

    first commit

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,37 @@
+import { Meteor } from 'meteor/meteor';
+import React, { useState } from 'react';
+//import { TasksCollection } from '/imports/db/TasksCollection';
+
+//export const TaskForm = ({user}) => {
+export const TaskForm = () => {
+    const [text, setText] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if(!text) return;
+
+        Meteor.call('tasks.insert', text);
+
+        // TasksCollection.insert({
+        //     text: text.trim(),
+        //     createdAt: new Date(),
+        //     userId: user._id
+        // });
+
+        setText("");
+    };
+
+    return(
+        <form className='task-form' onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder='Type to add new tasks'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <button type='submit'>Add Task</button>
+        </form>
+    );
+};
